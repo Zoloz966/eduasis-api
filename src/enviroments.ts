7c3af3d4f3dd192263f878e189904c916773cb2c
zoloz966 <zrodrigo96@gmail.com>
2024-09-19 03:24:00 -0400

    we add environmets and main

---
+++ src/enviroments.ts
@@ -0,0 +1,5 @@
+export const enviroments = {
+  dev: '.env',
+  test: '.stag.env',
+  prod: '.prod.env',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('VMax API')
+    .setDescription('Documentación API edu-sis')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  // URL API
+  SwaggerModule.setup('docs', app, document);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
