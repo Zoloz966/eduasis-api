728efd3115998b9784b5e8a1d8d4efb499c58096
zoloz966 <zrodrigo96@gmail.com>
2024-09-19 03:43:00 -0400

    we create open ai module

---
+++ src/openAi/openAi.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UserContextModule } from 'src/userContext/userContext.module';
+import { UserLogs } from 'src/users/entities/userLog.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([ UserLogs, ]),
+    UserContextModule,
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class OpenAiModule {}
